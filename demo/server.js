2629e056274ef04414d030c3e051d8608330b9b9
towersxu <987427795@qq.com>
2016-03-04 18:23:13 +0800

    in

--- demo/server.js
+++ demo/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded());
 app.use('/bower_components', express.static(__dirname + '/../bower_components'));
 app.use('/src', express.static(__dirname + '/../src'));
 
-app.get('/uptoken', function(req, res, next) {
+app.get('/v/uptoken', function(req, res, next) {
     var token = uptoken.token();
     res.header("Cache-Control", "max-age=0, private, must-revalidate");
     res.header("Pragma", "no-cache");
@@ -79,7 +79,7 @@ qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
 qiniu.conf.SECRET_KEY = config.SECRET_KEY;
 
 var uptoken = new qiniu.rs.PutPolicy(config.Bucket_Name);
-
+uptoken.persistentOps = config.fops;
 
 app.listen(config.Port, function() {
     console.log('Listening on port %d', config.Port);
